dd31082a593755e0a5f3d0bce59eeacb0b765643
git <claytnmorais@gmail.com>
2024-03-22 00:38:03 -0300

    apis com integração com keycloak

--- src/main/java/com/shoesclick/api/order/Application.java
+++ src/main/java/com/shoesclick/api/order/Application.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+
+@EnableMethodSecurity
+@EnableWebSecurity
 @EnableKafka
 @SpringBootApplication
 @EnableFeignClients
---
+++ src/main/java/com/shoesclick/api/order/config/FeignClientInterceptor.java
@@ -0,0 +1,28 @@
+package com.shoesclick.api.order.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String TOKEN_TYPE = "Bearer";
+
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        requestTemplate.header(AUTHORIZATION_HEADER, String.format("%s %s", TOKEN_TYPE, getTokenJwt().getTokenValue()));
+    }
+
+    private Jwt getTokenJwt() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        return Jwt.class.cast(context.getAuthentication().getPrincipal());
+    }
+
+
+}
